d18ebe34570f13d81b146a353ac8c45cd9e3873a
Katleiah <katleiahramos@gmail.com>
2018-03-12 18:27:35 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -19,9 +19,9 @@ end
 
 def retrieval
   shipping_manifest = {
-                  "whale bone corsets" => "5",
-                  "porcelain vases" => "2",
-                  "oil paintings" => "3"
+    "whale bone corset" => 5,
+    "porcelain vase" => 2,
+    "oil painting" => 3
                  }
 
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
